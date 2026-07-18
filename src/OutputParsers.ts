de0af817c93ae1d0e464bb439b986e4d98050748
Rohith Appala <rohith@ziply.ai>
2026-07-18 21:55:25 +0530

    Add CommaSeparatedListOutputParser to OutputParsers.ts for enhanced functionality

--- src/OutputParsers.ts
+++ src/OutputParsers.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 // First install: npm install langchain zod @langchain/google-genai
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { StringOutputParser } from "@langchain/core/output_parsers";
+import { CommaSeparatedListOutputParser, StringOutputParser } from "@langchain/core/output_parsers";
 
 const model = new ChatGoogleGenerativeAI({
   model: "gemini-2.5-flash",
@@ -26,4 +26,20 @@ async function stringParser() {
   console.log(response);
 }
 
-stringParser();
+async function commaSeparatedParser() {
+  const prompt = ChatPromptTemplate.fromTemplate(
+    "Gimme the list of things to carry at {input} court.",
+  );
+
+  const parser = new CommaSeparatedListOutputParser();
+
+  const chain = prompt.pipe(model).pipe(parser);
+
+  const response = await chain.invoke({
+    input: "Badminton",
+  });
+
+  console.log(response);
+}
+
+commaSeparatedParser();
