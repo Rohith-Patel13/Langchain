08b576884ec917abea3e6344fbbb6f22c49379fe
Rohith Appala <rohith@ziply.ai>
2026-07-18 21:50:55 +0530

    Update dev script to use OutputParsers.ts and add OutputParsers implementation

---
+++ src/OutputParsers.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+
+// First install: npm install langchain zod @langchain/google-genai
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+const model = new ChatGoogleGenerativeAI({
+  model: "gemini-2.5-flash",
+  apiKey: process.env.GOOGLE_API_KEY!,
+});
+
+async function stringParser() {
+  const prompt = ChatPromptTemplate.fromTemplate(
+    "Write a short description about {product_name}.",
+  );
+
+  const parser = new StringOutputParser();
+
+  const chain = prompt.pipe(model).pipe(parser);
+
+  const response = await chain.invoke({
+    product_name: "Langchain",
+  });
+
+  console.log(response);
+}
+
+stringParser();
